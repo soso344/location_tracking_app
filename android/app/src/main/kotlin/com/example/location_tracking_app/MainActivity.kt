6edfecdfc8652e63a932a219aa87674ab8144886
root <root@localhost.localdomain>
2025-06-09 17:22:11 +0300

    hide app icon v2

--- android/app/src/main/kotlin/com/example/location_tracking_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/location_tracking_app/MainActivity.kt
@@ -4,15 +4,16 @@ import android.content.ComponentName
 import android.content.pm.PackageManager
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
     private val CHANNEL = "com.example.location_tracking_app/launcher"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
 
-        MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
             if (call.method == "hideLauncherIcon") {
                 val success = hideLauncherIcon()
                 if (success) {
